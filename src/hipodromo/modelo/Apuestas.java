e6af7f45ebcb30ec4fef45eee90c3f9c336e3e32
ialonsordaw1 <ialonsordaw1@gmail.com>
2021-05-28 10:25:50 +0200

    V_0.5

--- src/hipodromo/modelo/Apuestas.java
+++ src/hipodromo/modelo/Apuestas.java
@@ -1,9 +1,28 @@
 package hipodromo.modelo;
 
 public class Apuestas {
-	private int dineroApostado;
+	private static int dineroApostado;
+	private Jugador jugador;
+	private static int apos;
 	
-	public Apuestas (int dinero) {
+	public Apuestas(int dinero) {
 		this.dineroApostado = dinero;
+		jugador = new Jugador();
+		apos = jugador.apostar(3);
+	}
+
+	public static void main(String args[]) {
+		calcularGanador(3, 10);
+	}
+	
+	public static double calcularGanador(int ganador, int ganadores) {
+		double resultado = 0;
+		if (apos == ganador) {
+			resultado = (dineroApostado * 0.75) / ganadores;
+			if (dineroApostado <= resultado) {
+				resultado = (dineroApostado * 1.25);
+			}
+		}
+		return resultado;
 	}
 }
---
+++ src/hipodromo/modelo/Jugador.java
@@ -0,0 +1,7 @@
+package hipodromo.modelo;
+
+public class Jugador {
+	public int apostar(int cab) {
+		return cab;
+	}
+}
